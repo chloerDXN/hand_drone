dbbc20222c4985a92b2f84b55895f1ade7830b89
chloerDXN <dickson.chloe@gmail.com>
2018-12-02 20:13:15 +0100

    watch interface with sensor value displayed (no sensor data yet)

--- wear/src/main/java/epfl/handdrone/FlyingActivity.java
+++ wear/src/main/java/epfl/handdrone/FlyingActivity.java
@@ -1,13 +1,48 @@
 package epfl.handdrone;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class FlyingActivity extends AppCompatActivity {
+
+public class FlyingActivity extends WearableActivity {
+
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private PowerManager mPowerManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get an instance of the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        // Get an instance of the PowerManager
+        mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+
+
         setContentView(R.layout.activity_flying);
     }
+
+    public void clickedStopFlyingXmlCallback(View view) {
+        Intent intent = new Intent(this, LauncherActivity.class);
+        startActivity(intent);
+    }
+
 }
+
+
--- wear/src/main/java/epfl/handdrone/LauncherActivity.java
+++ wear/src/main/java/epfl/handdrone/LauncherActivity.java
@@ -1,18 +1,19 @@
 package epfl.handdrone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class LauncherActivity extends WearableActivity {
 
     private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
 
         mTextView = (TextView) findViewById(R.id.text);
 
@@ -21,7 +22,10 @@ public class MainActivity extends WearableActivity {
     }
 
     public void clickedTakeOffXmlCallback(View view) {
-        TextView textView = findViewById(R.id.TakeOffMessage)   ;
+        TextView textView = findViewById(R.id.TakeOffMessage);
         textView.setText("Ready to take off!");
+
+        Intent intent = new Intent(this, FlyingActivity.class);
+        startActivity(intent);
     }
 }
